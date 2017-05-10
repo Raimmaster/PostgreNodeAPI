b92139e72105be6c9e911ae28fc7b83b170ad3dc
Nexer Rodríguez <Nexer Rodrguez>
2017-05-10 14:13:39 -0600

    List all

--- app.js
+++ app.js
@@ -17,24 +17,24 @@ app.use(bodyParser.json())
 var connectionsCredentials = [];
 var connections = [];
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send("Hello world");
 });
 
 //QUERIES
-app.post('/query', function(req, res){
+app.post('/query', function (req, res) {
     let id = 0;
     let sql = req.body.sql;
-    if(connections.length  == 0)
+    if (connections.length == 0)
         res.status(500).json("No connections availables");
-    else{
+    else {
         let client = connections[id];
-        
-        var query = client.query(sql, function(err, result){
-            if(err){
+
+        var query = client.query(sql, function (err, result) {
+            if (err) {
                 console.log(err);
                 res.status(500).json(err);
-            }else{
+            } else {
                 res.status(200).json(result.rows);
             }
         });
@@ -49,11 +49,11 @@ app.post('/query', function(req, res){
 });
 
 //CONNECTIONS
-app.get('/connections',function(req, res){
+app.get('/connections', function (req, res) {
     res.status(200).json(connectionsCredentials);
 });
 
-app.post('/connections',function(req, res){
+app.post('/connections', function (req, res) {
     let credentials = req.body.credentials;
 
     let connString = `pg://${credentials.username}:${credentials.password}@${credentials.host}:${credentials.port}/${credentials.db}`;
@@ -61,42 +61,42 @@ app.post('/connections',function(req, res){
     // let connString = `pg://${credentials.username}:${credentials.password}@${credentials.host}:${credentials.port}`;
     let client = new pg.Client(connString);
 
-    client.connect(function(err){
-        if(err)
+    client.connect(function (err) {
+        if (err)
             res.status(500).json(err);
-        else{
+        else {
             credentials.password = '********';
             connectionsCredentials.push(credentials);
             connections.push(client);
 
-            res.status(200).json("Connection added");    
+            res.status(200).json("Connection added");
         }
     });
 });
 
-app.put('/connections/:id',function(req, res){
+app.put('/connections/:id', function (req, res) {
     res.status(200).json("success");
 });
 
-app.delete('/connection:id',function(req,res){
+app.delete('/connection:id', function (req, res) {
     let id = req.params.id;
-    if(id >= connections.length)
+    if (id >= connections.length)
         res.status(500).json("Connection not found");
-    else{
+    else {
         let client = connections[id];
         client.end();
         connections.splice(id, 1);
         connectionsCredentials.splice(id, 1);
-    }        
+    }
 });
 
 //SESSIONS
-app.get("/sessions", function(req,res){
-    if(connections.length  == 0)
+app.get("/sessions", function (req, res) {
+    if (connections.length == 0)
         res.status(500).json("No connections availables");
-    else{
+    else {
         let client = connections[0];
-        
+
         var query = client.query("select * from pg_stat_activity;");
         query.on("row", function (row, result) {
             result.addRow(row);
@@ -108,41 +108,41 @@ app.get("/sessions", function(req,res){
 });
 
 //DATABASES
-app.get("/connection/:id/databases", function(req, res){
+app.get("/connection/:id/databases", function (req, res) {
     let id = 0;
-    if(this.credentials.length == 0)
+    if (this.credentials.length == 0)
         res.status(500).json("No connections availables");
-    else{
+    else {
         let cred = this.credentials[id];
         let client = this.connections[id];
 
         let sql = `select datname from pg_database pd inner join pg_user pu on pd.datdba = pu.usesysid where pu.usename = ${cred.username}`;
 
-        client.query(sql, function(err, result){
-            if(err){
+        client.query(sql, function (err, result) {
+            if (err) {
                 res.status(500).json(err);
-            }else{
-                res.status(200).json(result.rows);
+            } else {
+                res.status(200).json(result.rows.map(x => x.datname));
             }
         });
     }
 });
 
-app.post("/connection/:id/databases/:dbname", function(req, res){
+app.post("/connection/:id/databases/:dbname", function (req, res) {
     let id = 0;
     let dbname = req.params.dbname;
-    if(this.credentials.length == 0)
+    if (this.credentials.length == 0)
         res.status(500).json("No connections availables");
-    else{
+    else {
         let cred = this.credentials[id];
         let client = this.connections[id];
 
         let sql = `create database ${dbname} owner ${cred.username}`;
 
-        client.query(sql, function(err, result){
-            if(err){
+        client.query(sql, function (err, result) {
+            if (err) {
                 res.status(500).json(err);
-            }else{
+            } else {
                 console.log(result);
                 res.status(200).json('Database created');
             }
@@ -151,48 +151,189 @@ app.post("/connection/:id/databases/:dbname", function(req, res){
 });
 
 //TABLES
-app.get('/connection/:id/tables', function(req, res){
+app.get('/connection/:id/tables', function (req, res) {
     let id = 0;
-    let dbname = req.params.dbname;
-    if(this.credentials.length == 0)
+    if (this.credentials.length == 0)
         res.status(500).json("No connections availables");
-    else{
+    else {
         let cred = this.credentials[id];
         let client = this.connections[id];
 
         let sql = `select tablename from pg_tables where owner = ${cred.username}`;
 
-        client.query(sql, function(err, result){
-            if(err){
+        client.query(sql, function (err, result) {
+            if (err) {
                 res.status(500).json(err);
-            }else{
+            } else {
                 console.log(result);
-                res.status(200).json('Database created');
+                res.status(200).json(result.rows.map(x => x.tablename));
+            }
+        });
+    }
+});
+
+//INDEXES
+app.get('/connection/:id/indexes', function (req, res) {
+    let id = 0;
+    if (this.credentials.length == 0)
+        res.status(500).json("No connections availables");
+    else {
+        let cred = this.credentials[id];
+        let client = this.connections[id];
+
+        let sql = `select indexname from pg_indexes pi inner join pg_tables pt on pi.tablename = pt.tablename where pt.tableowner = ${cred.username}`;
+
+        client.query(sql, function (err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                console.log(result);
+                res.status(200).json(result.rows.map(x => x.indexname));
+            }
+        });
+    }
+});
+
+//CHECKS
+app.get('/connection/:id/checks', function (req, res) {
+    let id = 0;
+    if (this.credentials.length == 0)
+        res.status(500).json("No connections availables");
+    else {
+        let cred = this.credentials[id];
+        let client = this.connections[id];
+
+        let sql = `select 
+                    pci.conname 
+                    from pg_constraint pci 
+                    inner join pg_namespace pn on pci.connamespace = pn.oid 
+                    inner join pg_user pu on pu.usesysid = pn.nspowner
+                    where pci.contype = 'c' and pu.usename = ${cred.username}`;
+
+        client.query(sql, function (err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                console.log(result);
+                res.status(200).json(result.rows.map(x => x.conname));
             }
         });
     }
 });
 
 //FUNCTIONS
-app.get('/connection/:id/functions/:fn_name/ddl', function(req, res){
+app.get('/connection/:id/functions', function (req, res) {
+    let id = 0;
+    if (this.credentials.length == 0)
+        res.status(500).json("No connections availables");
+    else {
+        let cred = this.credentials[id];
+        let client = this.connections[id];
+
+        let sql = `select proname from pg_proc pp inner join pg_user pu on pu.usesysid = pp.proowner where pu.usename = ${cred.username}`;
+
+        client.query(sql, function (err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                console.log(result);
+                res.status(200).json(result.rows.map(x => x.proname));
+            }
+        });
+    }
+});
+
+app.get('/connection/:id/functions/:fn_name/ddl', function (req, res) {
     let id = 0;
     let fn_name = req.params.fn_name;
-    if(this.credentials.length == 0)
+    if (this.credentials.length == 0)
         res.status(500).json("No connections availables");
-    else{
+    else {
         let cred = this.credentials[id];
         let client = this.connections[id];
 
         let sql = `select fn_get_function_definition(${fn_name});`;
 
-        client.query(sql, function(err, result){
-            if(err){
+        client.query(sql, function (err, result) {
+            if (err) {
                 res.status(500).json(err);
-            }else{
+            } else {
                 res.status(200).json(result.rows);
             }
         });
     }
 });
 
+//VIEWS
+app.get('/connection/:id/views', function (req, res) {
+    let id = 0;
+    if (this.credentials.length == 0)
+        res.status(500).json("No connections availables");
+    else {
+        let cred = this.credentials[id];
+        let client = this.connections[id];
+
+        let sql = `select viewname from pg_views where viewowner = ${cred.username}`;
+
+        client.query(sql, function (err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                console.log(result);
+                res.status(200).json(result.rows.map(x => x.viewname));
+            }
+        });
+    }
+});
+
+//USERS
+app.get('/connection/:id/users', function (req, res) {
+    let id = 0;
+    if (this.credentials.length == 0)
+        res.status(500).json("No connections availables");
+    else {
+        let cred = this.credentials[id];
+        let client = this.connections[id];
+
+        let sql = `select usename from pg_user`;
+
+        client.query(sql, function (err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                console.log(result);
+                res.status(200).json(result.rows.map(x => x.usename));
+            }
+        });
+    }
+});
+
+//TRIGERS
+app.get('/connection/:id/trigers', function (req, res) {
+    let id = 0;
+    if (this.credentials.length == 0)
+        res.status(500).json("No connections availables");
+    else {
+        let cred = this.credentials[id];
+        let client = this.connections[id];
+
+        let sql = `select tgname from pg_trigger ptr inner join pg_class pc on pc.oid = ptr.tgrelid inner join pg_user pu on pu.usesysid = pc.relowner where pu.usename = ${cred.username}`;
+
+        client.query(sql, function (err, result) {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                console.log(result);
+                res.status(200).json(result.rows.map(x => x.tgname));
+            }
+        });
+    }
+});
+
 app.listen(8000);
+
+/*
+--select * from pg_tables pt inner join pg_namespace pn on pt.schemaname = pn.nspname limit 10;
+--select * from pg_namespace limit 10;
+
+*/
